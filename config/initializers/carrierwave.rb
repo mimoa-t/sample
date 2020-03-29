a23a86ce435848fe52b1d57bd0b12d56e4477ee8
mimoa-t <mimoa.toki@gmail.com>
2020-03-30 06:56:11 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'freemarketsample61a' 
-    config.asset_host = 'https://s3-your_region.amazonaws.com/freemarketsample61a'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample61a'
   else
     config.storage :file 
     config.enable_processing = false if Rails.env.test? 
